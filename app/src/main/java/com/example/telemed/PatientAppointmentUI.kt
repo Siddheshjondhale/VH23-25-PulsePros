628c2654642020025bc7618cf12da89fe84edba4
Siddhesh Jondhale <84581657+Siddheshjondhale@users.noreply.github.com>
2023-10-07 12:50:53 +0530

    doctor and pateint appointment boooked

--- app/src/main/java/com/example/telemed/PatientAppointmentUI.kt
+++ app/src/main/java/com/example/telemed/PatientAppointmentUI.kt
@@ -1,37 +1,20 @@
 package com.example.telemed
 
-import android.R
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import com.example.telemed.databinding.FragmentDoctorMainPro4Binding
+import android.widget.Toast
 import com.example.telemed.databinding.FragmentPatientAppointmentUIBinding
 import com.google.firebase.firestore.FirebaseFirestore
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_DOCTOR_EMAIL = "doctor_email"
 
 class PatientAppointmentUI : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentPatientAppointmentUIBinding
     private val db = FirebaseFirestore.getInstance()
-
-
-    lateinit var binding: FragmentPatientAppointmentUIBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,80 +22,101 @@ class PatientAppointmentUI : Fragment() {
     ): View {
         binding = FragmentPatientAppointmentUIBinding.inflate(inflater, container, false)
 
-
-
         val spinnerTimeSlot = binding.spinnerTimeSlot
-
-        // Set up the Spinner adapter
-        val timeSlotOptions = arrayOf("9:00 AM - 10:00 AM", "10:00 AM - 11:00 AM", "11:00 AM - 12:00 PM", "2:00 PM - 3:00 PM", "3:00 PM - 4:00 PM")
-        val adapter = ArrayAdapter(requireContext(), R.layout.simple_spinner_item, timeSlotOptions)
+        val timeSlotOptions = arrayOf(
+            "9:00 AM - 10:00 AM",
+            "10:00 AM - 11:00 AM",
+            "11:00 AM - 12:00 PM",
+            "2:00 PM - 3:00 PM",
+            "3:00 PM - 4:00 PM"
+        )
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, timeSlotOptions)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerTimeSlot.adapter = adapter
 
+        val submitButton = binding.BookAppointment
+        submitButton.setOnClickListener {
+            // Handle the button click here
+            val patientName = binding.patName.text.toString()
+            val patientEmail = binding.patEmail.text.toString()
+            val patientProblem = binding.patProblem.text.toString()
+            val selectedTimeSlot = binding.spinnerTimeSlot.selectedItem.toString()
 
+            // Retrieve the doctor's email from the arguments
+            val doctorEmail = arguments?.getString(ARG_DOCTOR_EMAIL)
 
-            val submitButton = binding.BookAppointment
-
-            submitButton.setOnClickListener {
-                // Handle the button click here
-                val patientName = binding.patName.text.toString()
-                val patientEmail = binding.patEmail.text.toString()
-                val patientProblem = binding.patProblem.text.toString()
-                val selectedTimeSlot = binding.spinnerTimeSlot.selectedItem.toString()
-
-                // Create and store the data in Firestore
-                storeAppointmentData(patientName, patientEmail, patientProblem, selectedTimeSlot)
-            }
-
-
+            // Create and store the patient's appointment data in Firestore
+            storePatientAppointment(patientName, patientEmail, patientProblem, selectedTimeSlot, doctorEmail)
+        }
 
         return binding.root
     }
 
+    private fun storePatientAppointment(
+        patientName: String,
+        patientEmail: String,
+        patientProblem: String,
+        selectedTimeSlot: String,
+        doctorEmail: String?
+    ) {
+        // Create a map to store the patient's appointment data
+        val appointmentData = hashMapOf(
+            "name" to patientName,
+            "email" to patientEmail,
+            "problem" to patientProblem,
+            "timeSlot" to selectedTimeSlot
+        )
 
-private fun storeAppointmentData(
-    patientName: String,
-    patientEmail: String,
-    patientProblem: String,
-    selectedTimeSlot: String
-) {
-    // Create a map to store the patient's appointment data
-    val appointmentData = hashMapOf(
-        "name" to patientName,
-        "email" to patientEmail,
-        "problem" to patientProblem,
-        "timeSlot" to selectedTimeSlot
-    )
-    // Retrieve the doctor's email from the arguments
-    val doctorEmail = arguments?.getString(ARG_PARAM1)
+        // Add the appointment data to Firestore under the patient's profile
+        db.collection("users")
+            .document("Patients")
+            .collection("Profile")
+            .document(patientEmail)
+            .collection("Appointments")
+            .document(doctorEmail.toString())
+            .set(appointmentData)
+            .addOnSuccessListener {
+                Toast.makeText(requireContext() , "Appointment Sucessfully Booked", Toast.LENGTH_SHORT).show()
+                // Data has been successfully stored in Firestore
+                // You can display a success message or navigate to another fragment/activity
+            }
+            .addOnFailureListener { e ->
+                // Handle any errors that may occur during the Firestore operation
+                // You can display an error message or log the error
+            }
 
-    // Add the appointment data to Firestore
-    db.collection("users")
-        .document("Patients")
-        .collection("Profile")
-        .document(patientEmail).collection("Appointments").document(doctorEmail.toString())
-        .set(appointmentData)
-        .addOnSuccessListener {
-            // Data has been successfully stored in Firestore
-            // You can display a success message or navigate to another fragment/activity
+        // Update the doctor's Firestore database with the patient's appointment details
+        if (doctorEmail != null) {
+            val doctorAppointmentData = hashMapOf(
+                "patientEmail" to patientEmail,
+                "patientName" to patientName,
+                "problem" to patientProblem,
+                "timeSlot" to selectedTimeSlot
+            )
+
+            db.collection("users")
+                .document("Doctors")
+                .collection("profile")
+                .document(doctorEmail)
+                .collection("Appointments")
+                .document(patientEmail)
+                .set(doctorAppointmentData)
+                .addOnSuccessListener {
+                    // Data has been successfully stored in the doctor's Firestore
+                }
+                .addOnFailureListener { e ->
+                    // Handle any errors that may occur during the Firestore operation
+                }
         }
-        .addOnFailureListener { e ->
-            // Handle any errors that may occur during the Firestore operation
-            // You can display an error message or log the error
-        }
-}
-
-
+    }
 
     companion object {
         @JvmStatic
         fun newInstance(doctorEmail: String) =
             PatientAppointmentUI().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, doctorEmail) // Store the doctor's email in the arguments
+                    putString(ARG_DOCTOR_EMAIL, doctorEmail) // Store the doctor's email in the arguments
                 }
             }
     }
-
-
 }
